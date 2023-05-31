e4efea04d601b57b8e89a99ce9d5e30e26aba0fe
HogHordGit <hoghord@gmail.com>
2023-06-01 01:45:15 +0300

    add service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-currency-convecter';
+  
+  public getJsonValue: any;
+  public postJsonValue: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getData();
+  }
+  
+  public getData() {
+    this.http.get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json').subscribe(data => {
+      console.log(data);
+      this.getJsonValue = data;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ConvertComponent } from './components/convert/convert.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConvertComponent
+    ConvertComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
